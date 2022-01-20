a0c8a44ebeff232501c8146177aac7b58043e426
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-01-20 12:05:45 +0530

    files updated

---
+++ index.js
@@ -0,0 +1,60 @@
+let hr = 0;
+let min = 0;
+let sec = 0;
+let count = 0;
+let timer = false;
+function start() {
+  timer = true;
+  stopwatch();
+}
+function stop() {
+  timer = false;
+}
+function reset() {
+  timer = false;
+  hr = 0;
+  min = 0;
+  sec = 0;
+  count = 0;
+  document.getElementById("hr").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("count").innerHTML = "00";
+}
+function stopwatch() {
+  if (timer == true) {
+    count = count + 1;
+    if (count == 100) {
+      sec = sec + 1;
+      count = 0;
+    }
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+    let hrstr = hr;
+    let secstr = sec;
+    let minstr = min;
+    let countstr = count;
+    if(hr <10){
+        hrstr = "0" + hr;
+    }
+    if(min<10){
+        minstr = "0" + min;
+    }if(sec<10){
+        secstr = "0" + sec;
+    }if(count<10){
+        countstr = "0" + count;
+    }
+    document.getElementById("hr").innerHTML = hrstr;
+    document.getElementById("min").innerHTML = minstr;
+    document.getElementById("sec").innerHTML = secstr;
+    document.getElementById("count").innerHTML = countstr;
+    setTimeout("stopwatch()", 10);
+  }
+}
